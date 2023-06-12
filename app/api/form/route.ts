fe3466f0c6e0a87aaf5458c413cd86c447112cf9
fhub-1 <flutterhub1.21.0@gmail.com>
2023-06-12 17:48:18 +0300

    adding some staff

---
+++ app/api/form/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse, NextRequest } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest): Promise<NextResponse<unknown>> {
+  try {
+    const { name, email, message, company } = await request.json();
+
+    // Save the form submission to the database
+    const submission = await prisma.dataForm.create({
+      data: {
+        name,
+        email,
+        message,
+        company,
+      },
+    });
+
+    return new NextResponse(JSON.stringify({ submission }), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Error submitting contact form:', error);
+    return new NextResponse(null, { status: 500 });
+  }
+}
---
+++ app/homepage/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+    <div>welocme to client with prisma form and next js 13 </div>
+    <Link href="/">back</Link>
+    </>
+  )
+}
+
+export default HomePage
